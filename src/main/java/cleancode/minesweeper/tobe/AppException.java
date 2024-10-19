9e220d77d6286216ccd1bf6976b22557c40e9d68
dayeon <99ekdus@naver.com>
2024-10-19 18:02:20 +0900

    2024-10-19

--- src/main/java/cleancode/minesweeper/tobe/AppException.java
+++ src/main/java/cleancode/minesweeper/tobe/AppException.java
@@ -1,6 +1,6 @@
 package cleancode.minesweeper.tobe;
 
-// 우리가 만든 프로그램 내에서 발생하는 의도한 예외
+// 우리가 만든 프로그램 내에서 발생하는 의도한 예외!
 public class AppException extends RuntimeException {
     // runtimeexception을 상속으로 받음
     public AppException (String message) {
